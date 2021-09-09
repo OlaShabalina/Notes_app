c814394df46c3db3b6149664eee6469852e9116b
OlaShabalina <o.shabalina@outlook.com>
2021-09-09 21:37:39 +1000

    Added event listeners to buttons and checkboxes

--- notes-functions.js
+++ notes-functions.js
@@ -1,29 +1,58 @@
-// Functions: 
-
 //  read existing notes from local storage
 
 const getSavedNotes = function () {
-    const notesJSON = localStorage.getItem('notes');
+    const notesJSON = localStorage.getItem('notes')
 
     if (notesJSON !== null) {
-        return JSON.parse(notesJSON);
+        return JSON.parse(notesJSON)
     } else {
-        return [];
-    };
+        return []
+    }
 }
+
+//  Remove a note from the list 
+
+const removeNote = function (id) {
+    const noteIndex = notes.findIndex((note) => {
+        return note.id === id;
+    });
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1);
+    }
+};
+
 
 // Generate the DOM structure for a note
 
 const generateNote = function (note) {
-    const noteEl = document.createElement('p');
+    const noteEl = document.createElement('div');
+    const noteButton = document.createElement('button');
+    const noteText = document.createElement('span');
 
+    // set up the remove note button
+    noteButton.textContent = 'x'
+    noteEl.appendChild(noteButton);
+    noteButton.addEventListener('click', function() {
+        removeNote(note.id);
+        saveNotes();
+        renderNotes(notes, filters);
+    })
+
+    // set up the note title text
     if (note.title.length > 0) {
-        noteEl.textContent = note.title;
+        noteText.textContent = note.title;
     } else {
-        noteEl.textContent = "Unnamed note";
+        noteText.textContent = "Unnamed note";
     };
 
+    noteEl.appendChild(noteText);
+
     return noteEl;
+}
+
+// Save the notes to localStorage
+const saveNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
 }
 
 // Render application notes
